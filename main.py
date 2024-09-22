9a7f56224cc763f7d45fc91f3783347dac637f18
Ikechukwu Okoro <ikeshegs@gmail.com>
2024-09-22 03:29:33 +0100

    day 2

--- main.py
+++ main.py
@@ -20,6 +20,12 @@ def find_post(id):
     for p in my_posts:
         if p['id'] == id:
             return p
+        
+
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
 
 
 @app.get("/")
@@ -48,4 +54,29 @@ def get_post(id: int, response: Response):
         # response.status_code = status.HTTP_404_NOT_FOUND
         # return {'message': f"Post with {id} was not found"}
     return {"post_detail": post}
+    
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_post(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=F"Post with id: {id} does not exist")
+
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    index = find_index_post(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} not found")
+    
+    post_dict = post_dict()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+
+    return {'data': post_dict}
     
