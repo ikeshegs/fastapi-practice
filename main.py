fdd3df9022149df85391bc919a31ce4e025d588d
Ikechukwu Okoro <ikeshegs@gmail.com>
2024-09-22 02:12:19 +0100

    day 2

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-from fastapi import Body, FastAPI
+from typing import Optional
+from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 
 @app.get("/")
@@ -13,8 +23,8 @@ def get_posts():
     return {"data": "This is your first post!"}
 
 
-@app.post("/createposts")
-def create_post(payload: dict = Body(...)):
-    print(payload)
-    return {"message": "Successfully created a new post"}
+@app.post("/posts")
+def create_post(post: Post):
+    print(post)
+    return {"message": post}
     
