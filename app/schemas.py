5bf8bcdda66baeaaaeebbd644a1192efe983871f
Ikechukwu Okoro <ikeshegs@gmail.com>
2024-09-24 16:18:06 +0100

    add create user schema, model, & route

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from datetime import datetime
 
 
@@ -14,6 +14,21 @@ class CreatePost(PostBase):
 
 class PostResponse(PostBase):
     id: int
+    created_at: datetime
+
+
+    class Config:
+        orm_mode: True
+
+
+class CreateUser(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class CreateUserResponse(BaseModel):
+    id: int
+    email: EmailStr
     created_at: datetime
 
 
