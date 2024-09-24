1571b178890a8d2165bdb28fd1fc993ef0692a72
Ikechukwu Okoro <ikeshegs@gmail.com>
2024-09-24 17:53:25 +0100

    add route tags to improve the docs readability

--- app/routers/post.py
+++ app/routers/post.py
@@ -2,11 +2,12 @@ from typing import List
 from fastapi import Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
 
-from .. import models, schemas, utils
+from .. import models, schemas
 from ..database import engine, get_db
 
 router = APIRouter(
-    prefix="/posts"
+    prefix="/posts",
+    tags=["Posts"]
 )
 
 @router.get("/", response_model=List[schemas.PostResponse])
