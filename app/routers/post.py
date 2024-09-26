f6208d7dc259c2666e26df0426775075c62543ef
Ikechukwu Okoro <ikeshegs@gmail.com>
2024-09-26 14:35:23 +0100

    Add authorization to all posts route

--- app/routers/post.py
+++ app/routers/post.py
@@ -11,7 +11,7 @@ router = APIRouter(
 )
 
 @router.get("/", response_model=List[schemas.PostResponse])
-def get_all_posts(db: Session = Depends(get_db)):
+def get_all_posts(db: Session = Depends(get_db), ser_id: int = Depends(oauth2.get_current_user)):
     # cursor.execute(""" SELECT * FROM posts """)
     # posts = cursor.fetchall()
 
@@ -35,7 +35,7 @@ def create_post(post: schemas.CreatePost, db: Session = Depends(get_db), user_id
 
 
 @router.get("/{id}", response_model=schemas.PostResponse)
-def get_one_post(id: int, db: Session = Depends(get_db)):
+def get_one_post(id: int, db: Session = Depends(get_db), ser_id: int = Depends(oauth2.get_current_user)):
     # cursor.execute(""" SELECT * FROM posts WHERE id = %s """, (str(id),))
     # post = cursor.fetchone()
     post = db.query(models.Post).filter(models.Post.id == id).first()
@@ -48,7 +48,7 @@ def get_one_post(id: int, db: Session = Depends(get_db)):
     
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db)):
+def delete_post(id: int, db: Session = Depends(get_db), ser_id: int = Depends(oauth2.get_current_user)):
     # cursor.execute(""" DELETE FROM posts WHERE id = %s returning * """, (str(id),))
     # deleted_post = cursor.fetchone()
     # conn.commit()
@@ -64,7 +64,7 @@ def delete_post(id: int, db: Session = Depends(get_db)):
 
 
 @router.put("/{id}", response_model=schemas.PostResponse)
-def update_post(id: int, updated_post: schemas.CreatePost, db: Session = Depends(get_db)):
+def update_post(id: int, updated_post: schemas.CreatePost, db: Session = Depends(get_db), ser_id: int = Depends(oauth2.get_current_user)):
     # cursor.execute(""" UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING * """, (post.title, post.content, post.published, str(id),))
     # updated_post = cursor.fetchone()
     # conn.commit()
