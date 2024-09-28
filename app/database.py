4be5fc09725184db365735019ed9da814685e1fc
Ikechukwu Okoro <ikeshegs@gmail.com>
2024-09-28 08:41:08 +0100

    code cleanup

--- app/database.py
+++ app/database.py
@@ -1,6 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
 
 DB_CONNECTION_URL = 'postgresql://postgres:C00ljoe.@localhost/fastapi-practice'
 
@@ -17,3 +20,15 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+# while True:
+#     try:
+#         conn = psycopg2.connect(host='localhost', database='fastapi-practice', user='postgres', password='C00ljoe.', cursor_factory=RealDictCursor)
+#         cursor = conn.cursor()
+#         print("Database connection was established successfully")
+#         break
+#     except Exception as error:
+#         print("Connecting to database failed")
+#         print("Error: ", error)
+#         time.sleep(2)
